f5fe372c922f20513984b2d52e7d5b14a2ff2952
Mateusz Karpinski <mateusz.karpinski29@gmail.com>
2024-08-20 14:30:18 +0200

    add install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-if [ -f ~/ .zshrc]; then
-  echo `alias cd='echo "$PWD" >> ~/.cd_log.txt; builtin cd'` >> ~/.zshrc
+mv cdb /usr/bin/cdb
+chmod 777 /usr/bin/cdb
 
-if [ -f ~/ .bashrc]; then
-  echo `alias cd='echo "$PWD" >> ~/.cd_log.txt; builtin cd'` >> ~/.bashrc
+if [ -f ~/.zshrc ]; then
+  echo "alias cd='echo \"\$PWD\" >> ~/.cd_log.txt; builtin cd'" >> ~/.zshrc
+fi
 
+if [ -f ~/.bashrc ]; then
+  echo "alias cd='echo \"\$PWD\" >> ~/.cd_log.txt; builtin cd'" >> ~/.bashrc
+fi
