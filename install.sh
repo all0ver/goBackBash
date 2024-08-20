5f76a7bf3f72f5bcb8a04b44d62fbaea9d3b62b7
Mateusz Karpinski <mateusz.karpinski29@gmail.com>
2024-08-20 16:53:42 +0200

    fix dir numbers

--- install.sh
+++ install.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
+cp cb /usr/bin/cb
+chmod 755 /usr/bin/cb  
 
-# Move and set permissions for the binary
-mv cb /usr/bin/cb
-chmod 755 /usr/bin/cb  # More secure permission setting
-
-# Define the function to be added
 read -r -d '' CD_FUNCTION << 'EOF'
 # Ensure no conflicting alias
 unalias cd 2>/dev/null
@@ -22,25 +19,17 @@ cd() {
 }
 EOF
 
-# Function to add the definition to a file
 add_to_file() {
   local file=$1
   local content=$2
-  
-  if ! grep -Fxq "$content" "$file"; then
-    echo "$content" >> "$file"
-    echo "Function added to $file"
-  else
-    echo "Function already exists in $file"
-  fi
+  echo "$content" >> "$file"
+  echo "Function added to $file"
 }
 
-# Add function to .zshrc if it exists
 if [ -f ~/.zshrc ]; then
   add_to_file ~/.zshrc "$CD_FUNCTION"
 fi
 
-# Add function to .bashrc if it exists
 if [ -f ~/.bashrc ]; then
   add_to_file ~/.bashrc "$CD_FUNCTION"
 fi
